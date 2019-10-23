feca6b478c4fc2435f3478ab98ecafe480a1f6dd
Pavel Pristupa <pristupa@gmail.com>
2019-10-23 20:30:12 +0700

    Lock annotations reading

--- pyannotations/annotations.py
+++ pyannotations/annotations.py
@@ -1,21 +1,24 @@
 import inspect
 import threading
+from typing import Dict
+from typing import Iterable
 from typing import List
-from typing import Tuple
 from typing import Type
 
 
 class Annotations:
     def __init__(self):
-        self._annotations_of_class = {}
-        self._classes_by_annotation_name = {}
+        self._annotations_of_class: Dict[Type, List[str]] = {}
+        self._classes_by_annotation_name: Dict[str, List[Type]] = {}
         self._lock = threading.Lock()
 
-    def get_annotations_of_class(self, cls: Type) -> Tuple[str]:
-        return tuple(self._annotations_of_class[cls])
+    def get_annotations_of_class(self, cls: Type) -> Iterable[str]:
+        with self._lock:
+            return tuple(self._annotations_of_class[cls])
 
-    def get_classes_by_annotation_name(self, annotation_name: str) -> List[Type]:
-        return list(self._classes_by_annotation_name[annotation_name])
+    def get_classes_by_annotation_name(self, annotation_name: str) -> Iterable[Type]:
+        with self._lock:
+            return list(self._classes_by_annotation_name[annotation_name])
 
     def annotate_class(self, cls: Type, annotation_name: str):
         with self._lock:
@@ -27,8 +30,8 @@ class Annotations:
             self._classes_by_annotation_name[annotation_name].append(cls)
 
 
-def annotate(annotation_name):
-    def wrapper(cls):
+def annotate(annotation_name: str):
+    def wrapper(cls: Type):
         if not inspect.isclass(cls):
             raise TypeError('Only classes can be annotated')
         annotations.annotate_class(cls, annotation_name)
